32a83e2377da2c8028841a13fd191bb051cef530
Mr-Chenzf <83440187+Mr-Chenzf@users.noreply.github.com>
2021-05-04 15:50:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -76,6 +76,52 @@
 using namespace std;
 using namespace cv;
 double redcontours, greencontours, yellowcontours;
+int port(int op)
+{
+	//串口部分代码
+	int fd;
+	struct termios options, newstate;
+	char buff[1];
+	char* buf = new char[5];//分配内存空间
+	fd = open("/dev/ttyUSB0", O_RDWR | O_NONBLOCK | O_NOCTTY | O_NDELAY);    //打开串口
+	if (fd == -1)
+		printf("can not open the COM!\n");
+	else
+		printf("open COMß ok!\n");
+
+	if (fcntl(fd, F_SETFL, 0) < 0) //改为阻塞模式
+		printf("fcntl failed\n");
+	else
+		printf("fcntl=%d\n", fcntl(fd, F_SETFL, 0));
+	//获取串口
+	tcgetattr(fd, &options);
+	//设置波特率
+	cfsetispeed(&options, B115200);
+	cfsetospeed(&options, B115200);
+	//串口设置
+	options.c_cflag |= (CLOCAL | CREAD);
+	options.c_cflag &= ~PARENB;//设置无奇偶校验位，N
+	options.c_cflag &= ~CSTOPB; //设置停止位1
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS8; //设置数据位
+	options.c_cc[VTIME] = 0;//阻塞模式的设置
+	options.c_cc[VMIN] = 1;
+	//激活新配置
+	tcsetattr(fd, TCSANOW, &options);
+
+	cout << buf[0] << endl;
+	sprintf(buf, "%c", op);
+	write(fd, buf, sizeof(buf));
+	tcflush(fd, TCIOFLUSH);//清除所有正在发送的I/O数据
+	read(fd, buf, size_t(buf));
+	cout << buf << endl;
+
+	read(fd, buff, sizeof(buff));
+	cout << buff << endl;
+	sleep(0.5);
+
+	return buf[0];
+}
 void red(Mat srcImage)
 {
 	Mat midImage;
